3c85fd2e56311b23d7574010311e96f02b82aebe
Denver Smith <denverpsmith@gmail.com>
2017-04-24 23:04:38 -0600

    Create side nav mockup

--- src/containers/transactions.jsx
+++ src/containers/transactions.jsx
@@ -11,6 +11,25 @@ import storage from '../utils/localStorageUtils';
 import TransactionCategorizer from '../components/transactionList';
 
 
+const column = {
+  margin: "0 0 5px 10px",
+  padding: "5px",
+  border: "1px solid #000"
+};
+
+
+const filterTransactions = (transaction, filterValue) => {
+  switch (filterValue) {
+    case 'all':
+      return true;
+    case 'categorized':
+      return transaction.categorized;
+    case 'uncategorized':
+      return !transaction.categorized;
+    default:
+      return true;
+  }
+}
 
 const mapTransactions = (transactions) => {
   return transactions.map(obj => {
@@ -29,6 +48,9 @@ const mapTransactions = (transactions) => {
 class Transactions extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentFilter: 'uncategorized'
+    };
     this.authToken = storage.get('auth-token');
   }
 
@@ -41,6 +63,29 @@ class Transactions extends Component {
     const uncategorizedTransactions = mapTransactions(
       this.props.transactions.filter(t => !t.ignore && !t.categorized)
     );
+
+    const filteredTransactions = this.props.transactions
+      .filter((t) => filterTransactions(t, this.state.currentFilter));
+
+    return (
+      <div>
+        <h4>Transactions</h4>
+        <div style={{display: "flex"}}>
+          <div style={column} onClick={() => this.setState({ currentFilter: 'all' })}>All</div>
+          <div style={column} onClick={() => this.setState({ currentFilter: 'categorized' })}>Categorized</div>
+          <div style={column} onClick={() => this.setState({ currentFilter: 'uncategorized' })}>Uncategorized {uncategorizedTransactions.length}</div>
+        </div>
+        {(() => {
+          return filteredTransactions.map(t =>
+              <div key={t.id} style={{border: "1px solid #ddd", padding: "5px", margin: "5px"}}>
+                <span>{t.name}&nbsp;</span>
+                <span>{t.amount}</span>
+              </div>
+          );
+        })()}
+      </div>
+    );
+    /*
     return (
       <div>
         {this.props.isFetching ?
@@ -54,6 +99,7 @@ class Transactions extends Component {
         }
       </div>
     );
+    */
   }
 }
 
